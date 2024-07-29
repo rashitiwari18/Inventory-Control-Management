fca23e58a99a8c8b4b1eee3bff1e29a212f038c8
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-07-29 18:39:33 +0530

    Update Item.sql

--- Item.sql
+++ Item.sql
@@ -1,1 +1,5 @@
-                          ---CREATING ITEM MASTER TABLE---
+ CREATE TABLE ITEM
+(i_code varchar2(8) primary key,
+i_name char(10) not null,
+i_description char(50),
+i_price int(10));                          ---CREATING ITEM MASTER TABLE---
