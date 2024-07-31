6cf53de0f6bc95f477b90a1baad76ffaeff5fc25
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-07-31 21:54:00 +0530

    Update 03-Project.sql

--- 03-Project.sql
+++ 03-Project.sql
@@ -1,1 +1,7 @@
                                                         ---CREATING PROJECT MASTER TABLE---
+CREATE TABLE PROJECT
+ (p_code varchar2(8) primary key,
+ p_name char(15)not null,
+ p_manager char(15),
+ p_location char(50),
+ p_city char(15));
