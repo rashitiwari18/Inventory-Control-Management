9b855fb01028729e3cf627da024ccfffa5f0324b
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-08-08 21:13:11 +0530

    Update Material Requisition Receipt[Header].sql

--- Material Requisition Receipt[Header].sql
+++ Material Requisition Receipt[Header].sql
@@ -1,2 +1,15 @@
                                           ---MATERIAL REQUISITION RECEIPT (MRR)---
                                                    --MRR HEADER--
+
+
+CREATE TABLE T_MRR_HDR
+(TMRRH_NO varchar2(20) constraint TMRRH_NO primary key,
+TMRRH_DATE date default sysdate,
+TMRRH_SUPPLIER_CODE varchar2(15) constraint TMRRH_SUPPLIER_CODE references SUPPLIER(s_id) not null,
+TMRRH_PRJ_CODE constraint TMRRH_PRJ_CODE references PROJECT(p_code) not null,
+TMRRH_DELV_NO varchar2(20),
+TMRRH_DELV_DATE date,
+TMRRH_ENT_BY varchar2(30) default user,
+TMRRH_ENT_DATE date default sysdate,
+TMRRH_UPDATE_BY varchar2(30) default user,
+TMRRH_UPDATE_DATE date default sysdate);
