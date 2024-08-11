5bcc5298b72d2350ddb1b3a06b52adefa2850fc3
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-08-10 19:40:45 -1200

    Update Material Requisition [ Detail ].sql

--- Material Requisition [ Detail ].sql
+++ Material Requisition [ Detail ].sql
@@ -1,1 +1,12 @@
                                                           --MRR DETAIL--
+
+CREATE TABLE  T_MRR_DTL  
+(TMRRD_NO varchar2(20) references T_MRR_HDR(TMRRH_NO),
+TMRRD_DATE date default sysdate,
+TMRRD_ITEM_CODE varchar2(30) constraint TMRRD_item_code_fk references item(i_code),
+TMRRD_UNIT varchar2(10) constraint TMRRD_unit_nn not null,
+TMRRD_UNIT_PRICE number(16,2) constraint TMRRD_unit_price_nn not null,
+TMRRD_ENT_BY varchar2(30) default user,
+TMRRD_ENT_DATE date default sysdate,
+TMRRD_UPDATE_BY varchar2(30) default user,
+TMRRD_UPDATE_DATE date default sysdate);
