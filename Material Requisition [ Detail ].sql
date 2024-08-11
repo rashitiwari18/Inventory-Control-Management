6d4066d0e49fa1742cd5722d252589c1b602b787
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-08-10 19:41:13 -1200

    Update Material Requisition [ Detail ].sql

--- Material Requisition [ Detail ].sql
+++ Material Requisition [ Detail ].sql
@@ -10,3 +10,16 @@ TMRRD_ENT_BY varchar2(30) default user,
 TMRRD_ENT_DATE date default sysdate,
 TMRRD_UPDATE_BY varchar2(30) default user,
 TMRRD_UPDATE_DATE date default sysdate);
+
+
+
+
+
+INSERT INTO T_MRR_DTL
+(TMRRD_NO ,
+TMRRD_DATE ,
+TMRRD_ITEM_CODE ,               
+TMRRD_UNIT ,
+TMRRD_UNIT_PRICE )
+VALUES
+('&TMRRD_NO',&TMRRD_DATE,'&TMRRD_I_CODE','&TMRRD_UNIT','&TMRRD_UN_PRC');
