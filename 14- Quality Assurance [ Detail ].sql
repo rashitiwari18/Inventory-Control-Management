ba6606949ef5c87d38aae5183b357bca1dbda171
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-08-21 23:39:53 +0530

    Update 14- Quality Assurance [ Detail ].sql

--- 14- Quality Assurance [ Detail ].sql
+++ 14- Quality Assurance [ Detail ].sql
@@ -1,1 +1,13 @@
                                                     --QA/QC DETAIL--
+
+CREATE TABLE  T_QC_DTL  
+(TQCD_NO varchar2(20) references  T_QC_HDR(TQCH_NO ),
+TQCD_DATE date default sysdate,
+TQCD_ITEM_CODE varchar2(30) constraint TQCD_item_code_fk references item(i_code),
+TQCD_UNIT varchar2(10) constraint TQCD_unit_nn not null,
+TQCD_UNIT_PRICE number(16,2) constraint TQCD_unit_price_nn not null,
+TQCD_ITEM_STATUS char(1) default 'N',
+TQCD_ENT_BY varchar2(30) default user,
+TQCD_ENT_DATE date default sysdate,
+TQCD_UPDATE_BY varchar2(30) default user,
+TQCD_UPDATE_DATE date default sysdate);
