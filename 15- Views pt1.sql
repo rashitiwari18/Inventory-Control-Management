0fae73b789f40f106c133e9f328e1c44d1910718
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-08-23 22:24:25 +0530

    Update 15- Views pt1.sql

--- 15- Views pt1.sql
+++ 15- Views pt1.sql
@@ -1,2 +1,7 @@
                                                        --- VIEWS ---
                                                         -ITEM VIEW--
+ CREATE OR REPLACE VIEW ITEM_VW
+ AS
+ SELECT * FROM ITEM;
+ 
+ SELECT * FROM ITEM_VW;
