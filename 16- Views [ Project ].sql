564d32ddf7f9553f1d11b08f81fde49a720ce93e
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-08-28 12:38:38 +0530

    Update 16- Views [ Project ].sql

--- 16- Views [ Project ].sql
+++ 16- Views [ Project ].sql
@@ -4,4 +4,4 @@ CREATE OR REPLACE VIEW PROJECT_VW
  AS
  SELECT * FROM PROJECT
  WHERE P_CITY='JAMSHEDPUR'
- WITH READ ONLY;                  // granted user cannot update any granted row  //   
+ WITH READ ONLY;                  // granted user cannot update any granted row  //     
