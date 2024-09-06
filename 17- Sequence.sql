eb307fee8ec8e69498cb8f9711232ded680feb55
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-09-05 22:26:41 -1100

    Update 17- Sequence.sql

--- 17- Sequence.sql
+++ 17- Sequence.sql
@@ -1,5 +1,4 @@
                                           --CURRENT_VAL--
                                            ---NEXT_VAL--
-
 SELECT PROJECT_SEQ.CURRVAL FROM DUAL;
 SELECT PROJECT_SEQ.NEXTVAL FROM DUAL;
