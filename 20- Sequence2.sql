46217405d460158275a264e13a6b3025f9fbeec4
Rashi Tiwari <166488281+rashitiwari18@users.noreply.github.com>
2024-09-15 21:18:02 -1200

    Update 20- Sequence2.sql

--- 20- Sequence2.sql
+++ 20- Sequence2.sql
@@ -1,4 +1,4 @@
-                                                             ----SEQUENCE--- 
+                                                            ----SEQUENCE--- 
 
  CREATE SEQUENCE  "HR"."PROJECT_SEQ"  MINVALUE 1 MAXVALUE 9990 INCREMENT BY 10 START WITH 100 NOCACHE  NOORDER  NOCYCLE ;  
  
